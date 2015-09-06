7a238c184d4eb0ee993a2936ba9e3427bf62e5a2
wghglory <wghglory89@gmail.com>
2015-09-06 12:23:35 -0400

    WCF

---
+++ WCFDemo/IBLL/IRoleService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBLL
+{
+    //1. 添加引用 System.ServiceModel.   interface add tag
+    [ServiceContract]
+    public interface IRoleService
+    {
+        [OperationContract]
+        UserInfo GetUser(int id);
+    }
+}
---
+++ WCFDemo/IBLL/UserInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Serialization;
+
+namespace IBLL
+{
+    //1. model add tag
+    [DataContract]
+    public class UserInfo
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+
+        [DataMember]
+        public string Name { get; set; }
+    }
+}
